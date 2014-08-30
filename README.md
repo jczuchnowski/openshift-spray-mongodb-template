20ac8b14fdd4ddb126c0417d1a508ed2ac5c550d
jczuchnowski <jakub.czuchnowski@gmail.com>
2014-08-31 00:39:28 +0200

    starting tutorial

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 openshift-spray-mongodb-template
 ================================
+
+Template for Spray/ReactiveMongo based application deployable to OpenShift PaaS.
+
+### Prerequisites:
+
+1. Creating an OpenShift account.
+2. Install OpenShift Client Tools.
+
+### Prepare your gear:
+
+~~~ bash
+rhc app create example-app jbossews-2.0
+~~~
+
+~~~ bash
+rhc cartridge add mongodb-2.4 -a example-app
+~~~
+
+~~~ bash
+rhc cartridge add rockmongo-1.1 -a example-app
+~~~
--- src/main/scala/spray/examples/DemoServiceActor.scala
+++ src/main/scala/spray/examples/DemoServiceActor.scala
@@ -26,55 +26,56 @@ object DemoServiceActor {
 class DemoServiceActor(repository: LogMessageRepository) extends HttpServiceActor with ServletDirectives {
 
   implicit val executionContext = context.dispatcher
-    
+  
   def receive = runRoute {
     schemeName { sName =>
       hostName { hName =>
         servletContextPath { srvPath =>
-        path("log" / Segment) { id =>
-          get {
-            complete {
-              for {
-                opt <- repository.get(id)
-              } yield opt.map { log =>
-                LogMessageData(log._id.stringify, log.msg, log.date)
-              }
-            }
-          }
-        } ~
-        path("log") {
-          get {
-            complete {
-              for {
-                seq <- repository.all
-              } yield seq.map { log =>
-                LogMessageData(log._id.stringify, log.msg, log.date)
-              }
-            }
-          } ~
-          post {
-            entity(as[LogMessageCmd]) { cmd =>
+          path("log" / Segment) { id =>
+            get {
               complete {
-                val newLogMsg = LogMessage(msg = cmd.msg, date = new DateTime)
-                val result = repository.save(newLogMsg)
-
                 for {
-                  err <- result
-                } yield {
-                  if (err.ok) {
-                    val baseHref = new URI(s"$sName://$hName$srvPath/log")
-                    val id = newLogMsg._id.stringify
-
-                    HttpResponse(status = StatusCodes.Created, headers = `Location`(s"$baseHref/$id") :: Nil)
-                  } else {
-                    val errMsg = err.errMsg.getOrElse("not ok")
-
-                    HttpResponse(StatusCodes.BadRequest, errMsg)
-                  }
+                  opt <- repository.get(id)
+                } yield opt.map { log =>
+                  LogMessageData(log._id.stringify, log.msg, log.date)
                 }
               }
             }
-          }}
+          } ~
+          path("log") {
+            get {
+              complete {
+                for {
+                  seq <- repository.all
+                } yield seq.map { log =>
+                  LogMessageData(log._id.stringify, log.msg, log.date)
+                }
+              }
+            } ~
+            post {
+              entity(as[LogMessageCmd]) { cmd =>
+                complete {
+                  val newLogMsg = LogMessage(msg = cmd.msg, date = new DateTime)
+                  val result = repository.save(newLogMsg)
+
+                  for {
+                    err <- result
+                  } yield {
+                    if (err.ok) {
+                      val baseHref = new URI(s"$sName://$hName$srvPath/log")
+                      val id = newLogMsg._id.stringify
+
+                      HttpResponse(status = StatusCodes.Created, headers = `Location`(s"$baseHref/$id") :: Nil)
+                    } else {
+                      val errMsg = err.errMsg.getOrElse("not ok")
+
+                      HttpResponse(StatusCodes.BadRequest, errMsg)
+                    }
+                  }
+                }
+              }
+            } 
+          }
         }
       }
     }
